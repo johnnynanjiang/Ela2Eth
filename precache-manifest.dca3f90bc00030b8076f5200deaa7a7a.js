871c16e969db61d8b47d0219a98f3f4120d777b9
Yushin Si <siyushin@hotmail.com>
2020-06-23 15:14:17 +0800

    Updates

---
+++ precache-manifest.dca3f90bc00030b8076f5200deaa7a7a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c4def41c868c66021b5c3c2f999b5d2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "660fd7fe8ad5dc7e80bc",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "da9a4fc399a8ef37af75",
+    "url": "/static/js/2.08f1ba97.chunk.js"
+  },
+  {
+    "revision": "bd32164b0b1f07ae79545cc1dff28ec5",
+    "url": "/static/js/2.08f1ba97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "660fd7fe8ad5dc7e80bc",
+    "url": "/static/js/main.9eb06b0e.chunk.js"
+  },
+  {
+    "revision": "4a42fdcbdfd3cf55fea3",
+    "url": "/static/js/runtime-main.1c47a842.js"
+  }
+]);
